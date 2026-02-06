d7d8d623c2806cf0c06e99af8d647b07ba5a8580
Shellbaka <shellbaka69@gmail.com>
2026-02-05 21:20:44 -0300

    adcionei novas pastas como hooks e services e criei a interface para uso e consumo das apis, agora irei importar as apis e fazer a estrutura de consumo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,4 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
 
 const Home: React.FC = () => {
   return (
@@ -16,7 +14,7 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+            Hello World
       </IonContent>
     </IonPage>
   );
